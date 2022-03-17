228baadb858c02fe719917b209c172fa9657aac0
jsalas603 <jsalas603@gmail.com>
2022-03-17 01:02:25 -0500

    worked with new methos that include .map, .filter, and .join to create new sections for about and projects

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 
+
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -134,12 +135,9 @@ Add a New Project
 
 promptUser()
   .then(promptProject)
-  .then(portfolioData => {
-    console.log(portfolioData);
-    // will be uncommented in lesson 4
-    // const pageHTML = generatePage(portfolioData);
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
+.then(portfolioData => {
+     const pageHTML = generatePage(portfolioData);
+     fs.writeFile('./index.html', pageHTML, err => {
+       if (err) throw new Error(err);
+     });
   });
