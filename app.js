71f49b1bd38f5cf1f0563f45493d17a9449be475
jsalas603 <jsalas603@gmail.com>
2022-03-20 22:03:41 -0500

    Created our own promises and used Promise-Chains

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 
@@ -135,9 +135,19 @@ Add a New Project
 
 promptUser()
   .then(promptProject)
-.then(portfolioData => {
-     const pageHTML = generatePage(portfolioData);
-     fs.writeFile('./index.html', pageHTML, err => {
-       if (err) throw new Error(err);
-     });
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
